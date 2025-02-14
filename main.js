40c3a7ffe9c99701eae11eec7522f68d143363f0
Edgartheprogrammer <eo.gh@hotmail.com>
2025-02-14 15:01:06 +0100

    remove box shadow from Moneda respecting the silhouette

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ class Moneda {
         this.element = document.createElement("img");
         this.element.classList.add("moneda");
         this.actualizarPosicion();
-        this.element.src = "images/queso.png"
+        this.element.src = "images/bad-cheese.png"
     }
     actualizarPosicion(){
         this.element.style.left = `${this.x}px`;
